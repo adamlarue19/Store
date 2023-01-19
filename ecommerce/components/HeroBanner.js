3624d12bfb4e1d10e950b900bc2ac49ba8207aa5
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2023-01-19 00:29:04 -0500

    added hero section and banner img

---
+++ ecommerce/components/HeroBanner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const HeroBanner = () => {
+  return (
+    <div className='Hero'>
+      <h1> HeroBanner </h1>
+
+      <div className='custom-img'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default HeroBanner
