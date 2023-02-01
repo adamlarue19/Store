5e515354984c1550e0e544dbf421b14bca8a70f1
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2023-02-01 12:50:52 -0500

    added checkout section to kid.js

--- ecommerce/pages/kid/[slug].js
+++ ecommerce/pages/kid/[slug].js
@@ -1,26 +1,87 @@
 import React from 'react'
 
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
 import { client, urlFor } from '../../lib/client';
+import  Kid  from '../../components/Kid'
+
 
 const KidDetails = ({ kid, kids, }) => {
 
-    const {image, name, details, price} = kid;
-    
+  const { image, name, details, price } = kid;
+
   return (
     <div>
       <div className='product-detail-container'>
         <div>
-          <div className='image-container'>
-            <img src={urlFor(image && image [0])} />
+          <div className='product-detail-imge'>
+            <img src={urlFor(image && image[0])} />
+          </div>
+          {/* <div className='small-images'>
+          {image?.map((item, i) => (
+            <img
+              src={urlFor(item)}
+              className=''
+              onMouseEnter=''
+            />
+          ))}
+        </div> */}
+        </div>
+        <div className='product-details-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>
+              (20)
+            </p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p>${price}</p>
+          <div className='quantity'>
+            <h3>Quantity: </h3>
+            <p className='quantity-desc'>
+              <span className='minus'
+              onClick=''><AiOutlineMinus />
+              </span>
+              <span className='num'
+              onClick=''>0</span>
+              <span className='plus'
+              onClick=''><AiOutlinePlus /> 
+              </span>
+            </p>
+          </div>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart'
+            onClick=''>Add to Cart</button>
+            <button type='button' className='buy-now' onClick=''>
+              Buy Now </button>
           </div>
         </div>
+      </div>
+      <div className='maylike-products-products-wrapper'>
+        <h2>You may also like</h2>
+        <div className='marquee'>
+          <div className=' maylike-products-container track'>
+            {kids.map((item) => (
+              <Kid key={item._id} kid={item} />
+            ))}
+          </div>
+        </div>
+
       </div>
     </div>
   )
 }
 
 export const getStaticPaths = async () => {
- 
+
   const kidQuery = `*[_type == "kid"] {
     slug {
       current
@@ -33,10 +94,10 @@ export const getStaticPaths = async () => {
     params: {
       slug: kid.slug.current
     }
-    
+
   }));
 
- 
+
   return {
     paths,
     fallback: 'blocking'
@@ -44,8 +105,8 @@ export const getStaticPaths = async () => {
 }
 
 
-export const getStaticProps = async ({params: {slug}}) => {
-  
+export const getStaticProps = async ({ params: { slug } }) => {
+
   const kidQuery = `*[_type == "kid" && slug.current == '${slug}'][0]`;
   const kidsQuery = '*[_type == "kid"]'
   const kid = await client.fetch(kidQuery);
@@ -54,7 +115,7 @@ export const getStaticProps = async ({params: {slug}}) => {
 
 
   return {
-    props: {  kid, kids  }
+    props: { kid, kids }
   }
 }
 
